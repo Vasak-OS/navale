a0d804ffe3c1a7e83fc02cf694cad248f3550f2d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-08-12 18:59:07 -0300

    Add Dock

---
+++ js/dock.js
@@ -0,0 +1,25 @@
+const win = nw.Window.get();
+
+// Set windows Properties
+win.setShowInTaskbar(false);
+win.moveBy(0,0);
+win.resizeTo(Math.round(screen.width), 34);
+win.setResizable(false);
+
+var idApp = execSynx('python', ["scripts/setDock.py"])
+  .stdout
+  .toString();
+
+exec('scripts/fixxprop',[idApp, Math.round(screen.width)])
+
+exec('python', ["scripts/setDock.py"]);
+iconMenu = execSynx('python', ["scripts/getIcon.py", "app-launcher"]).stdout.toString()
+
+document.getElementById("dockMenuLauncher").innerHTML = `
+<img
+  src="file://${iconMenu}"
+  class="img-fluid dock-menu-launcher-icon"
+  alt="Menu" />
+`;
+
+whritewindowsOpens()
---
+++ js/global.js
@@ -0,0 +1,53 @@
+const homePath = nw.App.dataPath.concat('/../../../');
+const execSynx = require('child_process').spawnSync;
+const exec = require('child_process').spawn;
+
+function openMenu(){
+  nw.Window.open("/menu/index.html",
+    {
+      "id": "ar.net.lynx.os.menu",
+      "new_instance": true,
+      "focus": true,
+      "title": "Lynx Menu",
+      "show": true,
+      "transparent": true,
+      "icon": "icon.svg",
+      "resizable": false,
+      "frame": false,
+      "position": "center",
+      "show_in_taskbar": false,
+      "width": 700,
+      "height": 620
+    })
+  whritewindowsOpens();
+}
+
+function setIMG(){
+  elem = document.getElementById('imgDiv');
+  elem.innerHTML = `
+    <img
+      src="file://${homePath}.face"
+      class="img-fluid lynx-profile-picture" />`
+}
+
+function whritewindowsOpens(){
+  windowsOpen = JSON.parse(execSynx('python', ["scripts/getWindowsOpen.py"])
+  .stdout
+  .toString()
+  .replaceAll('\'', ''));
+
+  elem = document.getElementById('windowsOpenSection');
+
+  html = ``
+
+  for (openwin in windowsOpen){
+    html = html.concat(`
+      <button class="btn win-button">
+        <img src="file://${windowsOpen[openwin].icon}" alt="${windowsOpen[openwin].icon}" class="img-fluid win-img" />
+      </button>
+    `)
+  }
+
+  elem.innerHTML = html
+
+}
