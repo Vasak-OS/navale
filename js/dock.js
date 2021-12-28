fc18bde36d893bf726aaa0986c363ac5c4a27f35
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-12-28 18:35:15 -0300

    Fix set Dock

--- js/dock.js
+++ js/dock.js
@@ -3,16 +3,17 @@ const win = nw.Window.get();
 // Set windows Properties
 win.setShowInTaskbar(false);
 win.resizeTo(Math.round(screen.width), 34);
-win.moveBy(0,64);
 win.setResizable(false);
+win.y = 0;
+win.x = 0;
 
-var idApp = execSynx('python', ["/usr/share/Lynx/lynx-dock/scripts/setDock.py", `${process.pid.toString()}`])
+var idApp = execSynx('python', ["/usr/share/Lynx/lynx-dock/scripts/setDock.py", `${process.pid.toString()}`, Math.round(screen.width)])
   .stdout
   .toString();
 
 console.log(idApp);
 
-exec('scripts/fixxprop',[idApp, Math.round(screen.width)])
+//exec('/usr/share/Lynx/lynx-dock/scripts/fixxprop',[idApp, Math.round(screen.width)])
 
 iconMenu = execSynx('python', ["/usr/share/Lynx/lynx-dock/scripts/getIcon.py", "app-launcher"]).stdout.toString()
 
--- scripts/setDock.py
+++ scripts/setDock.py
@@ -1,7 +1,11 @@
 import gi
 import sys
 gi.require_version('Wnck', '3.0')
-from gi.repository import Wnck
+gi.require_version("Gtk", "3.0")
+from gi.repository import Wnck, Gtk, Gdk
+import Xlib
+from Xlib.display import Display
+from Xlib import X
 
 screen = Wnck.Screen.get_default()
 screen.force_update()
@@ -12,4 +16,16 @@ for win in temp_windows:
     win.set_window_type(2)
     print(win.get_xid())
     win.make_above()
+
+    display = Display()
+    topw = display.create_resource_object('window', win.get_xid())
+    topw.change_property(display.intern_atom('_NET_WM_STRUT'),
+                       display.intern_atom('CARDINAL'), 32,
+                       [0, 0, 34, 0 ],
+                       X.PropModeReplace)
+    topw.change_property(display.intern_atom('_NET_WM_STRUT_PARTIAL'),
+                       display.intern_atom('CARDINAL'), 32,
+                       [0, 0, 34, 0, 0, 0, 0, 0, 0, int(sys.argv[2])-1, 0, 0],
+                       ## [0, 0, 34, 0, 0, 0, 0, 0, 0, 0, 0, int(sys.argv[2])-1], Para bottom
+                       X.PropModeReplace)
 
