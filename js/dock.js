8ac71e246845d0779138cb82d20d4153d790b2d4
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-22 18:31:19 -0300

    Add windows propertis in js fix '

--- js/dock.js
+++ js/dock.js
@@ -6,15 +6,16 @@ win.resizeTo(Math.round(screen.width), 36);
 win.setResizable(false);
 win.y = 0;
 win.x = 0;
+win.setAlwaysOnTop(true);
 
-exec('python', ["/usr/share/Lynx/lynx-desktop-service/Setters/setDock.py", `${process.pid.toString()}`, Math.round(screen.width)]);
+exec('python', ['/usr/share/Lynx/lynx-desktop-service/Setters/setDock.py', `${process.pid.toString()}`, Math.round(screen.width)]);
 
-setMenuButton("app-launcher");
+setMenuButton('app-launcher');
 
-async function setMenuButton(buttonicon = "app-launcher") {
-  iconMenu = execSynx('python', ["/usr/share/Lynx/lynx-desktop-service/Lynx/getIcon.py", buttonicon]).stdout.toString()
+async function setMenuButton(buttonicon = 'app-launcher') {
+  iconMenu = execSynx('python', ['/usr/share/Lynx/lynx-desktop-service/Lynx/getIcon.py', buttonicon]).stdout.toString()
 
-  document.getElementById("dockMenuLauncher").innerHTML = `
+  document.getElementById('dockMenuLauncher').innerHTML = `
   <img
     src="file://${iconMenu}"
     class="img-fluid dock-menu-launcher-icon"
