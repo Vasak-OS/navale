620058ac63d1bf8337e0be1b6094ccc0ffe861cf
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-25 15:44:57 -0300

    Implement new signar recived

--- js/desktopService.js
+++ js/desktopService.js
@@ -1,11 +1,26 @@
 const desktopServiceName = 'ar.net.lynx.os.desktop.service';
 const desktopInterfaceName = desktopServiceName;
 const desktopObjectPath = `/${desktopServiceName.replace(/\./g, '/')}`;
+const desktopServiceDBUS = sessionBus.getService(desktopServiceName);
+
+desktopServiceDBUS.getInterface(desktopObjectPath, desktopInterfaceName, (err, iface) => {
+  if (err) {
+    console.error(
+      `Failed to request interface '${desktopInterfaceName}' at '${desktopObjectPath}' : ${
+        err
+      }`
+        ? err
+        : '(no error)'
+    );
+    process.exit(1);
+  }
+  iface.on('updateWindows', function(){
+    writewindowsOpens(arguments[0]);
+  });
+});
 
 async function toggleWin(win){
-  const desktopServicePrev = sessionBus.getService(desktopServiceName)
-
-  desktopServicePrev.getInterface(desktopObjectPath, desktopInterfaceName, (err, iface) => {
+  desktopServiceDBUS.getInterface(desktopObjectPath, desktopInterfaceName, (err, iface) => {
     if (err) {
       console.error(
         `Failed to request interface '${desktopInterfaceName}' at '${desktopObjectPath}' : ${
