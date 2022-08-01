d4829cb40f3f62ae5145ddbc7e1768f7b4ec4144
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-08-01 15:45:36 -0300

    Add MenuButton

---
+++ js/components/MenuButtom.js
@@ -0,0 +1,23 @@
+export default {
+    methods:{
+        openMenu(){
+            this.$exec('lynx-menu');
+        }
+    },
+    computed:{
+        icon(){
+            return this.$execSynx('python', ['/usr/share/Lynx/lynx-desktop-service/Lynx/getIcon.py', 'lynx-menu']).stdout.toString();
+        }
+    },
+    template: `
+        <a class="navbar-brand" href="#" id="dockMenuLauncher" @click="openMenu()">
+        <img
+            :src="'file://' + icon"
+            data-bs-toggle="tooltip"
+            data-bs-placement="left"
+            title="Lynx Menu"
+            class="img-fluid dock-menu-launcher-icon"
+            alt="Menu" />
+        </a>
+    `
+}
