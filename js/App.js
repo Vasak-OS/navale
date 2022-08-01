3abda0c75bbe06ac4894762a8042d9dc1c38e6cb
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-08-01 16:02:27 -0300

    Update Global Properties Set Windows Properties in App Remove dock.js (all migrate to vue)

--- js/App.js
+++ js/App.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue';
 import MenuButtom from './components/MenuButtom.js';
 
 const app = createApp({
-    data() {
-        return {
-            message: 'Hello Vue!'
-        }
+    async beforeMount() {
+        // Set windows Properties
+        this.$win.setShowInTaskbar(false);
+        this.$win.resizeTo(Math.round(screen.width), 36);
+        this.$win.setResizable(false);
+        this.$win.y = 0;
+        this.$win.x = 0;
+        this.$win.setAlwaysOnTop(true);
+        this.$exec('python', ['/usr/share/Lynx/lynx-desktop-service/Setters/setDock.py', `${this.$pid.toString()}`, Math.round(this.$screen.width)]);
     },
     template: `
     <div class="container-fluid">
@@ -18,12 +23,15 @@ const app = createApp({
             <div id="clock"></div>
         </div>
     </div>`,
-    components:{
+    components: {
         MenuButtom
     }
 });
 
 app.config.globalProperties.$execSynx = require('child_process').spawnSync;
 app.config.globalProperties.$exec = require('child_process').spawn;
+app.config.globalProperties.$win = nw.Window.get();
+app.config.globalProperties.$pid = process.pid;
+app.config.globalProperties.$screen = screen;
 
 app.mount('#app');
