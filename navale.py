20aa4c9ca2ecca1496015507b437aa2a46c52fec
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-08-11 14:33:57 -0300

    move windows to lib

--- navale.py
+++ navale.py
@@ -1,63 +1,14 @@
 import sys
-import os
 import signal
-from PyQt5.QtCore import QUrl, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from Vasak.VSKDockWindow import VSKDockWindow
+   
 
 app = QApplication(sys.argv)
+app.setAttribute(Qt.AA_UseHighDpiPixmaps) # Usar pixeles de alta resolución
+app.setAttribute(Qt.AA_EnableHighDpiScaling) # Habilitar escalamiento de alta resolución
 
-class VSKDockWindow(QMainWindow):
-    def __init__(self, screen_num=0):
-        super().__init__()
-        self.set_attributes() # Establecer atributos de la ventana
-        self.move_to_screen(screen_num) # Mover la ventana a una pantalla específica
-        self.set_widget() # Establecer el widget central. Y el WebView.
-        self.set_webview_poprties()
-        self.load_html("index.html") # Cargar un HTML en el WebView
-    
-    # Mover la ventana a una pantalla específica
-    def move_to_screen(self, screen_num):
-        desktop = QApplication.desktop()
-        available_rect = desktop.availableGeometry(screen_num)
-        self.move(available_rect.topLeft())
-    
-    # Establecer atributos de la ventana
-    def set_attributes(self):
-        self.setWindowTitle("navale")
-        self.setGeometry(0, 0, QApplication.desktop().screenGeometry().width(), 40) # Establecer tamaño
-        self.setAttribute(Qt.WA_TranslucentBackground, True) # Fondo transparente
-        self.setAttribute(Qt.WA_NoSystemBackground, True) # No dibujar el fondo del sistema
-        self.setAttribute(Qt.WA_X11NetWmWindowTypeDock, True) # Seteo tipo dock x11
-        self.setAttribute(Qt.WA_AlwaysStackOnTop, True) # Mantener la ventana por encima de las demás
-        self.transparent = True # Establecer transparencia
-        self.setWindowFlags(
-            self.windowFlags() | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.CustomizeWindowHint 
-        )
-        app.setAttribute(Qt.AA_UseHighDpiPixmaps) # Usar pixeles de alta resolución
-        app.setAttribute(Qt.AA_EnableHighDpiScaling) # Habilitar escalamiento de alta resolución
-    
-    # Cargar un HTML en el WebView
-    def load_html(self, html):
-        file_path = os.path.abspath(html)
-        self.webview.webContent = file_path
-        self.webview.load(QUrl.fromLocalFile(file_path))
-        page = self.webview.page()
-        page.setBackgroundColor(Qt.transparent)
-        
-    def set_webview_poprties(self):
-        self.webview.setContextMenuPolicy(Qt.NoContextMenu)
-        settings = self.webview.settings()
-        settings.setAttribute(settings.JavascriptEnabled, True)
-        settings.setAttribute(settings.AllowWindowActivationFromJavaScript, True)
-        settings.setAttribute(settings.ShowScrollBars, False)
-        
-    # Establecer el widget central. Y el WebView.
-    def set_widget(self):
-        self.webview = QWebEngineView(self)
-        self.setCentralWidget(self.webview)
-        
-        
 if __name__ == "__main__":
     window = VSKDockWindow()
     window.show()
