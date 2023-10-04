41d8a4db1538e204b86f58623167e8fb3606ab86
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-10-04 09:35:58 -0300

    use primary screen

--- Navale/NavaleWindow.py
+++ Navale/NavaleWindow.py
@@ -7,15 +7,16 @@ from PyQt6.QtWebEngineWidgets import QWebEngineView
 
 
 class NavaleWindow(VSKWindow):
-    def __init__(self, screen_num=0):
+    def __init__(self):
         super().__init__()
-        self.move_to_screen(screen_num) # Mover la ventana a una pantalla específica
+        self.move_to_screen() # Mover la ventana a una pantalla específica
         self.set_as_dock() # Hacer que la ventana se comporte como un dock
         self.load_html("index.html") # Cargar un HTML en el WebView
         
     
     # Mover la ventana a una pantalla específica
-    def move_to_screen(self, screen_num):
+    def move_to_screen(self):
         self.setGeometry(0, 0, QApplication.primaryScreen().availableGeometry().width(), 40)
-        ##self.move(QApplication.primaryScreen().availableGeometry().topLeft())
+        self.setScreen(QApplication.primaryScreen())
+        self.move(QApplication.primaryScreen().availableGeometry().topLeft())
     
