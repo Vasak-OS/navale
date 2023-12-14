8d07275bdc3bc12dbb72cdd25497f6a8c81feb9b
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-14 14:09:26 -0300

    add storage to window

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import { windowsStore } from './stores/windows'
 
 const startApp = (channel: any) => {
   const app = createApp(App)
+
   app.config.globalProperties.$vsk = channel.objects.vsk
   app.use(createPinia())
+  // @ts-ignore
+  window["windowManager"] = windowsStore()
 
   app.mount('#app')
 }
